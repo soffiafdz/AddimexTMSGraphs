3f71d910383e758d85ab2ac071e83e138ddd6e9d
Fernandez Lozano <fernandezlozas2@nih.gov>
2019-04-10 14:51:33 -0400

    Updated databases

--- clinicalData.R
+++ clinicalData.R
@@ -1,24 +1,42 @@
 
 # Raw data  -----------------------------------------------------------------------------------
 
-data_amai <- read_excel('inData/clinical_data/Dic_2018/AMA INSE 8x7.xlsx', sheet = 2)
-data_asip <- read_excel('inData/clinical_data/Dic_2018/ASIP.xlsx', sheet = 1, na = 'NA')
-data_bis11 <- read_excel('inData/clinical_data/Dic_2018/BIS-11.xlsx', sheet = 2)
-data_calendar <- read_excel('inData/clinical_data/Dic_2018/Calendar.xlsx', sheet = 2, na = 'NA')
-data_cal2 <- read_excel('inData/clinical_data/Dic_2018/CalendarRelative.xlsx', sheet = 2, na = 'NA')
-data_ccqg <- read_excel('inData/clinical_data/Dic_2018/CCQ-G.xlsx', sheet = 1)
-data_ccqn <- read_excel('inData/clinical_data/Dic_2018/CCQ-N.xlsx', sheet = 1)
-data_ehi <- read_excel('inData/clinical_data/Dic_2018/EHI-SF.xlsx', sheet = 1)
-data_hars <- read_excel('inData/clinical_data/Dic_2018/HARS.xlsx', sheet = 2)
-data_hdrs <- read_excel('inData/clinical_data/Dic_2018/HDRS.xlsx', sheet = 2)
-data_inventory <- read_excel('inData/clinical_data/Dic_2018/Inventory.xlsx', sheet = 1, na = 'NA')
-data_mini <- read_excel('inData/clinical_data/Dic_2018/MINI PLUS.xlsx', sheet = 1)
-data_psqi <- read_excel('inData/clinical_data/Dic_2018/PSQI.xlsx', sheet = 1, na = 'NA')
-data_scl90 <- read_excel('inData/clinical_data/Dic_2018/SCL-90-R.xlsx', sheet = 1)
-data_tobacco <- read_excel('inData/clinical_data/Dic_2018/Tobacco.xlsx', sheet = 1)
-data_vas <- read_excel('inData/clinical_data/Dic_2018/VAS Clinico.xlsx', sheet = 1)
-data_whodas <- read_excel('inData/clinical_data/Dic_2018/WHODAS 2,0 X.xlsx', sheet = 1, na = 'NA')
-data_demog <- read_excel('inData/clinical_data/Dic_2018/DEMOGRAPHIC.xlsx', sheet = 1, na = 'NA')
+data_amai <- read_excel('inData/clinical_data/20190410/AMA INSE 8x7.xlsx',
+                        sheet = 2)
+data_asip <- read_excel('inData/clinical_data/20190410/ASIP.xlsx',
+                        sheet = 1, na = 'NA')
+data_bis11 <- read_excel('inData/clinical_data/20190410/BIS-11.xlsx',
+                         sheet = 2)
+data_calendar <- read_excel('inData/clinical_data/20190410/Calendar.xlsx',
+                            sheet = 2, na = 'NA')
+data_cal2 <- read_excel('inData/clinical_data/20190410/CalendarRelative.xlsx',
+                        sheet = 2, na = 'NA')
+data_ccqg <- read_excel('inData/clinical_data/20190410/CCQ-G.xlsx',
+                        sheet = 1)
+data_ccqn <- read_excel('inData/clinical_data/20190410/CCQ-N.xlsx',
+                        sheet = 1)
+data_ehi <- read_excel('inData/clinical_data/20190410/EHI-SF.xlsx',
+                       sheet = 1)
+data_hars <- read_excel('inData/clinical_data/20190410/HARS.xlsx',
+                        sheet = 2)
+data_hdrs <- read_excel('inData/clinical_data/20190410/HDRS.xlsx',
+                        sheet = 2)
+data_inventory <- read_excel('inData/clinical_data/20190410/Inventory.xlsx',
+                             sheet = 1, na = 'NA')
+data_mini <- read_excel('inData/clinical_data/20190410/MINI PLUS.xlsx',
+                        sheet = 1)
+data_psqi <- read_excel('inData/clinical_data/20190410/PSQI.xlsx',
+                        sheet = 1, na = 'NA')
+data_scl90 <- read_excel('inData/clinical_data/20190410/SCL-90-R.xlsx',
+                         sheet = 1)
+data_tobacco <- read_excel('inData/clinical_data/20190410/Tobacco.xlsx',
+                           sheet = 1)
+data_vas <- read_excel('inData/clinical_data/20190410/VAS Clinico.xlsx',
+                       sheet = 1)
+data_whodas <- read_excel('inData/clinical_data/20190410/WHODAS 2,0 X.xlsx',
+                          sheet = 1, na = 'NA')
+data_demog <- read_excel('inData/clinical_data/20190410/DEMOGRAPHIC.xlsx',
+                         sheet = 1, na = 'NA')
 
 
 # Demographics --------------------------------------------------------------------------------
@@ -33,44 +51,49 @@ sd <- left_join(data_demog, data_tobacco, by = c("rid", "group", "stage")) %>%
            cig_day = as.double(cig_day)) %>% 
     as.data.table()
 names(sd) <- sd_names <- c("RID", "Group", "Sex", "Age", "Education_years", 
-                           "Starting_age", "Years_consumption", "Tobacco_consumption", 
-                           "Tobacco_start", "Tobacco_years", "Cigs_day") 
+                           "Starting_age", "Years_consumption",
+                           "Tobacco_consumption", "Tobacco_start",
+                           "Tobacco_years", "Cigs_day") 
 
 
 # Clinical ------------------------------------------------------------------------------------
 
 clinical <- data_vas %>% 
-    left_join(data_ccqg[c(1:3, 49)]) %>% 
-    left_join(data_ccqn[c(1:3, 49)]) %>% 
-    left_join(data_bis11[c(1:3, 34:37)]) %>% 
-    left_join(data_inventory[c(1:2,4, 19:20, 23:24, 26, 28)]) %>% 
-    left_join(data_asip[c(1:3, 68)]) %>% 
-    left_join(data_cal2[c(1:3, 17)]) %>% 
-    filter(stage %in% c("T0", "T1", "T1-4", "T2")) %>% 
-    dplyr::rename(Study.ID = rid, Group = group, Stage = stage, B11_C = cog, 
-                  B11_M = mot, B11_NP = nonp, B11_Tot = tot_score, 
-                  CCQ_G = ccq_g, CCQ_N = ccq_n, VAS = vas, 
-                  UT_bzd = ut_bzd, UT_coc = ut_coc, UT_thc = ut_thc, 
-                  Consume = auto1, Coc_last30 = d8cc30, Grams0 = "gram_-1", 
-                  Grams_month = auto3, Improvement = auto5) %>% 
+    left_join(data_ccqg[c(1:3, 49)]) %>%
+    left_join(data_ccqn[c(1:3, 49)]) %>%
+    left_join(data_bis11[c(1:3, 34:37)]) %>%
+    left_join(data_inventory[c(1:2,4, 19:20, 23:24, 26, 28)]) %>%
+    left_join(data_asip[c(1:3, 68)]) %>%
+    # left_join(data_cal2[c(1:3, 17)]) %>%
+    filter(stage %in% c("T0", "T1", "T1-4", "T2")) %>%
+    dplyr::rename(Study.ID = rid, Group = group, Stage = stage, B11_C = cog,
+                  B11_M = mot, B11_NP = nonp, B11_Tot = tot_score,
+                  CCQ_G = ccq_g, CCQ_N = ccq_n, VAS = vas,
+                  UT_bzd = ut_bzd, UT_coc = ut_coc, UT_thc = ut_thc,
+                  Consume = auto1, Grams_month = auto3, Improvement = auto5, 
+                  Coc_last30 = d8cc30) %>% #, Grams0 = "gram_-1") %>%
     mutate(Study.ID = factor(paste0("sub-", sprintf("%03d", Study.ID))),
            Group = factor(Group, labels = c("sham", "tx")),
            Stage = factor(Stage, labels = c("t0", "t1", "t1-4", "t2")),
-           VAS = as.double(VAS), 
+           VAS = as.double(VAS),
            UT_bzd = factor(UT_bzd, labels = c("positive", "negative")),
            UT_coc = factor(UT_coc, labels = c("positive", "negative")),
            UT_thc = factor(UT_thc, labels = c("positive", "negative")),
            Consume = factor(Consume, labels = c("Yes", "No")),
-           Improvement = factor(Improvement, labels = c("Better", "Moderately better", 
-                                            "Slightly better", "No changes")), 
-           Grams_month = as.double(Grams_month),
-           Grams = case_when(
-               Consume == "No" ~ 0,
-               Consume == "Yes" ~ Grams_month,
-               is.na(Consume) ~ Grams0),
-           Grams = if_else(Coc_last30 == 0, 0, Grams)) %>% 
-    select(-Grams0, -Grams_month) %>% 
-    filter(!Study.ID %in% paste0("sub-0", c('05', '09', 14, 28, 29))) %>% data.table()
+           Improvement = factor(Improvement, labels = c("Better",
+                                                        "Moderately better",
+                                                        "Slightly better",
+                                                        "No changes",
+                                                        "Worse"))) %>% 
+    #        Grams_month = as.double(Grams_month),
+    #        Grams = case_when(
+    #            Consume == "No" ~ 0,
+    #            Consume == "Yes" ~ Grams_month,
+    #            is.na(Consume) ~ Grams0),
+    #        Grams = if_else(Coc_last30 == 0, 0, Grams)) %>%
+    # select(-Grams0, -Grams_month) %>%
+    filter(!Study.ID %in% paste0("sub-0", c('05', '09', 14, 28, 29))) %>%
+    data.table()
 
 
 # write_rds(clinical, "outData/clin_data.RDS")
@@ -96,7 +119,11 @@ clin2 <- clinical %>%
     data.table()
 
 clin3 <- clinical %>%
-    filter(Study.ID %in% as.character(clinical[clinical$Stage == "t2",]$Study.ID)) %>%
+    filter(
+        Study.ID %in% as.character(
+            clinical[clinical$Stage == "t2",]$Study.ID
+            )
+        ) %>%
     mutate(Stage2 = case_when(
         Group == "tx" & Stage == "t0" ~ "t0",
         Group == "tx" & Stage == "t1" ~ "t1",
@@ -119,15 +146,24 @@ write_rds(clin3, "outData/cl3.RDS")
 
 aovClin <- vector('list', length = 9)
 
-aovClin[[1]] <- aov_car(VAS ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[2]] <- aov_car(CCQ_N ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[3]] <- aov_car(CCQ_G ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[4]] <- aov_car(B11_C ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[5]] <- aov_car(B11_M ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[6]] <- aov_car(B11_NP ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[7]] <- aov_car(B11_Tot ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[8]] <- aov_car(Coc_last30 ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
-aovClin[[9]] <- aov_car(Grams ~ Group * Stage + Error(Study.ID/Stage), data = clin1)
+aovClin[[1]] <- aov_car(VAS ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[2]] <- aov_car(CCQ_N ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[3]] <- aov_car(CCQ_G ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[4]] <- aov_car(B11_C ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[5]] <- aov_car(B11_M ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[6]] <- aov_car(B11_NP ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[7]] <- aov_car(B11_Tot ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[8]] <- aov_car(Coc_last30 ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
+aovClin[[9]] <- aov_car(Grams ~ Group * Stage + Error(Study.ID/Stage),
+                        data = clin1)
 
 
 
@@ -152,15 +188,24 @@ write_rds(chiClin, "outData/chiClin.RDS")
 
 tTestClin <- vector('list', length = 2)
 
-tTestClin[[1]][[1]] <- t.test(VAS ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[2]] <- t.test(CCQ_N ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[3]] <- t.test(CCQ_G ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[4]] <- t.test(B11_C ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[5]] <- t.test(B11_M ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[6]] <- t.test(B11_NP ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[7]] <- t.test(B11_Tot ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[8]] <- t.test(Coc_last30 ~ Stage, alternative = "g", paired = T, data = clin2)
-tTestClin[[1]][[9]] <- t.test(Grams ~ Stage, alternative = "g", paired = T, data = clin2)
+tTestClin[[1]][[1]] <- t.test(VAS ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[2]] <- t.test(CCQ_N ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[3]] <- t.test(CCQ_G ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[4]] <- t.test(B11_C ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[5]] <- t.test(B11_M ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[6]] <- t.test(B11_NP ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[7]] <- t.test(B11_Tot ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[8]] <- t.test(Coc_last30 ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
+tTestClin[[1]][[9]] <- t.test(Grams ~ Stage, alternative = "g",
+                              paired = T, data = clin2)
 
 tTestClin[[2]][[1]] <- cohen.d(VAS ~ Stage, paired = T, data = clin2)
 tTestClin[[2]][[2]] <- cohen.d(CCQ_N ~ Stage, paired = T, data = clin2)
@@ -175,12 +220,9 @@ tTestClin[[2]][[9]] <- cohen.d(Grams ~ Stage, paired = T, data = clin2)
 
 chiClin2 <- vector('list', length = 3)
 
-chiClin2[[1]] <- chisq.test(table(filter(clin2)$UT_bzd, 
-                                 filter(clin2)$Stage))
-chiClin2[[2]] <- chisq.test(table(filter(clin2)$UT_coc, 
-                                 filter(clin2)$Stage))
-chiClin2[[3]] <- chisq.test(table(filter(clin2)$UT_thc, 
-                                 filter(clin2)$Stage))
+chiClin2[[1]] <- chisq.test(table(filter(clin2)$UT_bzd, filter(clin2)$Stage))
+chiClin2[[2]] <- chisq.test(table(filter(clin2)$UT_coc, filter(clin2)$Stage))
+chiClin2[[3]] <- chisq.test(table(filter(clin2)$UT_thc, filter(clin2)$Stage))
 
 write_rds(tTestClin, "outData/tTstClin.RDS")
 write_rds(chiClin2, "outData/chiClin2.RDS")
@@ -213,12 +255,9 @@ anovaClin[[2]][[9]] <- TukeyHSD(aov(Grams ~ Stage, data = clin3))
 
 chiClin3 <- vector('list', length = 3)
 
-chiClin3[[1]] <- chisq.test(table(filter(clin3)$UT_bzd, 
-                                  filter(clin3)$Stage))
-chiClin3[[2]] <- chisq.test(table(filter(clin3)$UT_coc, 
-                                  filter(clin3)$Stage))
-chiClin3[[3]] <- chisq.test(table(filter(clin3)$UT_thc, 
-                                  filter(clin3)$Stage))
+chiClin3[[1]] <- chisq.test(table(filter(clin3)$UT_bzd, filter(clin3)$Stage))
+chiClin3[[2]] <- chisq.test(table(filter(clin3)$UT_coc, filter(clin3)$Stage))
+chiClin3[[3]] <- chisq.test(table(filter(clin3)$UT_thc, filter(clin3)$Stage))
 
 write_rds(anovaClin, "outData/anovaClin.RDS")
 write_rds(chiClin3, "outData/chiClin3.RDS")
@@ -246,14 +285,22 @@ WB <- attrWB %>% left_join(clinical) %>% data.table()
 
 ## 2 weeks Real rTMS
 
-CONtx <- attrCONtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table() 
-DANtx <- attrDANtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-DMNtx <- attrDMNtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-FPNtx <- attrFPNtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-SALtx <- attrSALtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-SUBtx <- attrSUBtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-VANtx <- attrVANtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
-WBtx <- attrWBtx %>% left_join(clin2) %>% filter(Study.ID != 'sub-012') %>% data.table()
+CONtx <- attrCONtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table() 
+DANtx <- attrDANtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+DMNtx <- attrDMNtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+FPNtx <- attrFPNtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+SALtx <- attrSALtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+SUBtx <- attrSUBtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+VANtx <- attrVANtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
+WBtx <- attrWBtx %>% left_join(clin2) %>%
+    filter(Study.ID != 'sub-012') %>% data.table()
 
 ## 3 months maintenance
 
