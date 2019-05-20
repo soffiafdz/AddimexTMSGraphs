e56f760a443985ccc35434555367b45091769f15
soffiafdz <fernandezlozas2@nih.gov>
2019-05-20 16:20:47 -0400

    Format

--- setUp.R
+++ setUp.R
@@ -1,49 +1,53 @@
 
-# Packages and functions ----------------------------------------------------------------------
+# Packages and functions --------------------------------------------------
 
-pacman::p_load(brainGraph, data.table, tidyverse, reshape2, moonBook, xtable,
-               foreach, doParallel, afex, GGally, ggpubr, effsize, WRS2, readxl)
+pacman::p_load(
+  brainGraph, data.table, tidyverse, reshape2, moonBook, xtable,
+  foreach, doParallel, afex, GGally, ggpubr, effsize, WRS2, readxl
+)
 
-registerDoParallel(cores=1)
+registerDoParallel(cores = 1)
 
-source('customFunctions.R')
-
-
-# Location dependencies -----------------------------------------------------------------------
-
-#Tango
-home <- '/run/media/sofdez/Omega/TMS/derivatives/graphs/'
-
-#Huasteca
-# home <- '/media/neuroimagen/Omega/TMS/derivatives/graphs/'
+source("customFunctions.R")
 
 
 # Constants ---------------------------------------------------------------
 
-today <- format(Sys.Date(), '%m-%y')
-savedir <- paste0(home, 'brainGraph/')
+today <- format(Sys.Date(), "%Y%m%d")
+
+savedir <- "outData/graphs/"
 savedir1 <- paste0(savedir, today, "_")
-groups1 <- c('control', 'user')
-groups2 <- c('sham', 'tx')
+
+groups1 <- c("control", "user")
+groups2 <- c("sham", "tx")
+
 atlas <- "power"
-modality <- 'fmri'
+modality <- "fmri"
 thresholds <- 0.2
+
 covars <- fread("inData/participants.txt")
-    names(covars)[1] <- "Study.ID"
-    covars[, group := as.factor(group)]
-    covars[, sex := as.factor(sex)]
-    setkey(covars, group, Study.ID)
-    covars2 <- covars[-c(1, 8, 9, 12)]
-    covars3 <- covars2[-c(2, 4:6, 12, 14:15, 17, 21:23)]
-covars_p <- fread("inData/controls.txt") %>% 
-    dplyr::rename(Study.ID = participant_id) %>% 
-    mutate(sex = factor(sex, labels = c('M', 'F'))) %>% 
-    rbind(covars, fill = T) %>% data.table()
-    covars_p[, group := rep(c('control', 'user'), each = 27)]
-    covars_p[, group := as.factor(group)]
-    covars_p[, sex := as.factor(sex)]
-    setkey(covars_p, group, Study.ID)
-inds_p <- mk_inds(covars_p, groups1)
-inds <- mk_inds(covars, groups2)
-    inds2 <- mk_inds(covars2, groups2)
-    inds3 <- mk_inds(covars3, groups2)
+setnames(covars, 1, "Study.ID")
+covars[,`:=`(
+    group = as.factor(group),
+    sex = as.factor(sex)
+)]
+setkey(covars, group, Study.ID)
+
+covars2 <- covars[-c(1, 8, 9, 12)]
+covars3 <- covars2[-c(2, 4:6, 12, 14:15, 17, 21:23)]
+
+covarsP <- fread("inData/controls.txt")
+setnames(covars, 1, "Study.ID")
+covarsP[, sex := factor(sex, labels = c("M", "F"))]
+covarsP <- rbind(covarsP, covars, fill = T)
+covarsP[, `:=`(
+    group = factor(rep(groups1, each = 31)),
+    sex = as.factor(sex)
+)]
+setkey(covars_p, group, Study.ID)
+
+indsP <- mkInds(covars_p, groups1)
+inds <- mkInds(covars, groups2)
+
+inds2 <- mkInds(covars2, groups2)
+inds3 <- mkInds(covars3, groups2)
