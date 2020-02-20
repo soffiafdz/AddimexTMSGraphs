1e59d6997e5550916a2b079bbb72aa4d8191964d
soffiafdz <so1.618e@gmail.com>
2020-02-19 17:14:02 -0800

    first changes

--- setUp.R
+++ setUp.R
@@ -2,8 +2,19 @@
 # Packages and functions --------------------------------------------------
 
 pacman::p_load(
-  brainGraph, data.table, tidyverse, foreach, doParallel, readxl, plyr, lme4, 
-  multcomp, stargazer, effsize, moonBook, corrplot
+  brainGraph,
+  data.table,
+  tidyverse,
+  foreach,
+  doParallel,
+  readxl,
+  plyr,
+  lme4, 
+  multcomp,
+  stargazer,
+  effsize,
+  moonBook,
+  corrplot
 )
 
 registerDoParallel(cores = detectCores()/2)
@@ -23,47 +34,45 @@ groups2 <- c("Sham", "Tx")
 
 atlas <- "power264"
 modality <- "fmri"
-thresholds <- rev(seq(0.4, 0.01, -0.025))
+thresholds <- rev(seq(0.4, 0.01, -0.05))
 subThresh <- 0.5
 
-covars <- fread("inData/participants.csv", na.strings = 'NA')
-setnames(covars, 1, "Study.ID")
+covars <- fread("inData/participants.tsv", na.strings = 'NA')
 covars[,`:=`(
-  Group = factor(group, labels = c("Sham", "Tx")),
+  group = factor(group, labels = c("Sham", "Tx")),
   sex = factor(sex, labels = c("M", "F"))
-)][, group := NULL]
-setkey(covars, Group, Study.ID)
-
-covars1 <- covars[exclusion == 0]
-covars2 <- covars1[Study.ID %in% sprintf("sub-%03d",
-    c(2:4,7:8,15,20,22:25,27,31,32,34,36))]
-covars3 <- covars2[!Study.ID %in% sprintf("sub-%03d",
-    c(15,23,25,31,34))]
-
-covarsP <- fread("inData/participantsConn.csv", na.strings = c('n/a','NA'))
-covarsP <- covarsP[group == 1 & exclusion == 0]
-setnames(covars, 1, "Study.ID")
-covarsP <- covarsP[, `:=`(
-    sex = factor(sex, labels = c("M", "F")),
-    education = educ_yr
-)][,c(1:4,17,12:15)]
-covarsP <- rbind(covarsP, covars, fill = T)
-covarsP[, `:=`(
-  Group = factor(c(rep("HC", 45), rep("CU", 51))),
-  group = NULL,
-  sex = as.factor(sex),
-    exclusion = NULL
 )]
-setkey(covarsP, Group, Study.ID)
+setnames(
+  covars,
+  c(1:6), 
+  c("Age", "Educ", "Exclusion"))
+covars <- do.call("rbind", replicate(2, covars, simplify = F))
+covars[, Session := c(rep("T0", nrow(covars)/2), rep("T1", nrow(covars)/2))]
+setkey(covars, Session, Group, Study.ID)
 
-indsP <- lapply(seq_along(groups1), function(x)
-    covarsP[, which(Group == groups1[x])])
-inds1 <- lapply(seq_along(groups2), function(x)
-    covars1[, which(Group == groups2[x])])
-inds2 <- lapply(seq_along(groups2), function(x)
-    covars2[, which(Group == groups2[x])])
-inds3 <- lapply(seq_along(groups2), function(x)
-    covars3[, which(Group == groups2[x])])
+
+covars <- covars[Exclusion == 0]
+
+# covarsP <- fread("inData/participantsConn.tsv", na.strings = c('n/a','NA'))
+# covarsP <- covarsP[group == 1 & exclusion == 0]
+# setnames(covars, 1, "Study.ID")
+# covarsP <- covarsP[, `:=`(
+#     sex = factor(sex, labels = c("M", "F")),
+#     education = educ_yr
+# )][,c(1:4,17,12:15)]
+# covarsP <- rbind(covarsP, covars, fill = T)
+# covarsP[, `:=`(
+#   Group = factor(c(rep("HC", 45), rep("CU", 51))),
+#   group = NULL,
+#   sex = as.factor(sex),
+#     exclusion = NULL
+# )]
+# setkey(covarsP, Group, Study.ID)
+
+# indsP <- lapply(seq_along(groups1), function(x)
+    # covarsP[, which(Group == groups1[x])])
+inds <- lapply(seq_along(groups2), function(x)
+    covars[, which(Group == groups2[x])])
 
 # Atlas -------------------------------------------------------------------
 
@@ -83,12 +92,12 @@ power264[network == "Salience", networkLabel := "SAL"]
 power264[network == "Subcortical", networkLabel := "SUB"]
 power264[network == "Ventral attention", networkLabel := "VAN"]
 
-pCON <- power264[networkLabel == "CON"]
-pDAN <- power264[networkLabel == "DAN"]
-pDMN <- power264[networkLabel == "DMN"]
-pFPN <- power264[networkLabel == "FPN"]
-pSAL <- power264[networkLabel == "SAL"]
-pSUB <- power264[networkLabel == "SUB"]
-pVAN <- power264[networkLabel == "VAN"]
+# pCON <- power264[networkLabel == "CON"]
+# pDAN <- power264[networkLabel == "DAN"]
+# pDMN <- power264[networkLabel == "DMN"]
+# pFPN <- power264[networkLabel == "FPN"]
+# pSAL <- power264[networkLabel == "SAL"]
+# pSUB <- power264[networkLabel == "SUB"]
+# pVAN <- power264[networkLabel == "VAN"]
 
 
