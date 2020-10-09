a465bef2a9e79f83a444adedefca3b4c7cf35035
soffiafdz <so1.618e@gmail.com>
2020-10-09 12:40:13 -0700

    From table1 to tableby

--- scripts/tables.R
+++ scripts/tables.R
@@ -3,7 +3,8 @@
 ## Packages
 library(readr)
 library(data.table)
-library(table1)
+# library(table1)
+library(arsenal)
 library(here)
 
 ## Load data
@@ -15,22 +16,32 @@ levels(covars$Sex)      <- c("Male", "Female")
 levels(covars$Tx)[2:3]  <- c("Psych & Pharm", "Pharm")
 
 # Change some labels
-label(covars$Educ)      <- "Education"
-label(covars$Civ)       <- "Civil status"
-label(covars$Empl)      <- "Employment"
-label(covars$Substance) <- "Main substance"
-label(covars$Tobacco)   <- "Smoking"
-label(covars$Cigs_day)  <- "Cigarettes/day"
-label(covars$Tx)        <- "Treatment"
-label(covars$Ystart)    <- "Onset age of cocaine use"
-label(covars$Tcons)     <- "Years of cocaine use"
+labels(covars) <- c(
+  Age       = "Age (years)",
+  Educ      = "Education (years)",
+  Civ       = "Civil status",
+  Empl      = "Employment (last 3 y)",
+  Substance = "Main substance",
+  Tobacco   = "Smoking",
+  Cigs_day  = "Cigarettes/day",
+  Tx        = "Treatment",
+  Ystart    = "Onset age of cocaine use",
+  Tcons     = "Years of cocaine use"
+)
 
 # Units
-units(covars$Age)       <- "years"
-units(covars$Educ)      <- "years"
-units(covars$Empl)      <- "last 3y"
+# units(covars$Age)       <- "years"
+# units(covars$Educ)      <- "years"
+# units(covars$Empl)      <- "last 3y"
 
 # Table
+t1 <- tableby(Group ~ Sex + Age + Educ + Civ + Empl + Tobacco + Tobacco_years +
+  Cigs_day + Substance + Tx + Ystart + Tcons,
+  data = covars[Session == "T0"],
+  test = FALSE)
+
+summary(t1, text = TRUE)
+
 # table1(~ Sex + Age + Educ + Civ + Empl + Tobacco + Tobacco_years +
 #          Cigs_day + Substance + Tx + Ystart + Tcons | Group,
 #        data = covars[Session == "T0"], overall = "Total")
@@ -38,28 +49,38 @@ units(covars$Empl)      <- "last 3y"
 ## Clinic summary table
 
 # Labels
-label(covars$BIS)         <- "BIS-11"
-# label(covars$BISc)      <- "BIS-11 Cognitive"
-# label(covars$BISm)      <- "BIS-11 Motor"
-# label(covars$BISn)      <- "BIS-11 Non-planning"
-label(covars$VAS)         <- "Visual Analog Scale"
-# label(covars$CCQn)      <- "CCQ-Now"
-# label(covars$CCQg)      <- "CCQ-General"
-label(covars$Cons_Freq)   <- "Consumption"
-label(covars$Cons_Grams)  <- "Consumption"
-label(covars$FD)          <- "In-scanner Movement"
+labels(covars)   <- c(
+  BIS         = "BIS-11",
+  BISc        = "BIS-11 Cognitive",
+  BISm        = "BIS-11 Motor",
+  BISn        = "BIS-11 Non-planning",
+  VAS         = "Visual Analog Scale",
+  CCQn        = "CCQ-Now",
+  CCQg        = "CCQ-General",
+  Cons_Freq   = "Consumption (freq)",
+  Cons_Grams  = "Consumption (g)",
+  FD          = "In-scanner movement (mm)"
+)
 
 # Units
-units(covars$VAS)         <- "cm"
-units(covars$FD)          <- "mm"
-units(covars$Cons_Freq)   <- "freq"
-units(covars$Cons_Grams)  <- "g"
+# units(covars$VAS)         <- "cm"
+# units(covars$FD)          <- "mm"
+# units(covars$Cons_Freq)   <- "freq"
+# units(covars$Cons_Grams)  <- "g"
 
 # Table
+t2 <- tableby(Session ~ Sex + Age + Cons_Freq + Cons_Grams + Relapse + Status +
+  BIS + VAS + FD,
+  data = covars[Session %in% c("T0", "T1")],
+  strata = Group,
+  total = FALSE,
+  test = FALSE)
+
+summary(t2, text = TRUE)
+
 # table1(~ BIS + BISc + BISm + BISn + VAS + CCQn + CCQg + FD | Session*Group,
-table1(~ Sex + Age + Cons_Freq + Cons_Grams + Relapse + Status +
-         BIS + VAS + FD | Group*Session,
-       data = covars[Session %in% c("T0", "T1")], overall = FALSE)
+# table1(~ Sex + Age + Cons_Freq + Cons_Grams + Relapse + Status + BIS + VAS + FD | Group*Session,
+#        data = covars[Session %in% c("T0", "T1")], overall = FALSE)
 
 ## Longitudinal summary data
 gdlong <- copy(covars)
@@ -69,6 +90,14 @@ gdlong[Group == "Sham" & Session == "T14", Session := "T1"]
 gdlong[, Session := factor(Session, levels = paste0("T", c(-1, 0, 1, 2, 3)))]
 
 # Table
-table1(~ Sex + Age + Cons_Freq + Cons_Grams + Relapse + Status +
-         BIS + VAS + FD | Session,
-        data = gdlong, overall = FALSE)
+t3 <- tableby(Session ~ Sex + Age + Cons_Freq + Cons_Grams + Relapse +
+  Status + BIS + VAS + FD,
+  data = gdlong,
+  total = FALSE,
+  test = FALSE)
+
+summary(t3, text = TRUE)
+
+# table1(~ Sex + Age + Cons_Freq + Cons_Grams + Relapse + Status +
+#          BIS + VAS + FD | Session,
+#         data = gdlong, overall = FALSE)
