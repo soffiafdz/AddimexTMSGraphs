2a5e24016e26acf32d6f0266609e45650316ace2
soffiafdz <so1.618e@gmail.com>
2021-02-20 22:53:28 -0500

    Download several atlases [working]

--- scripts/download_mats_sge.sh
+++ scripts/download_mats_sge.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-rsync -avzhLe ssh \
+for gsig in gs ngs; do
+	[ $gsig = ngs ] && orig="graphs_ngs_20210205" || orig="graphs_20210205"
+	out="data/raw/timeseries/$gsig/"
+	mkdir -p $out
+	rsync -avzhLe ssh \
   --include "*/" --include "*ts.1D" --exclude "*" \
-  sfernandezl@ada.lavis.unam.mx:garzaLab/addimex_tms/derivatives/XCP/XCP_FEB2020 \
-  data/raw/timeseries \
+  ada:garzaLab/addimex_tms/derivatives/XCP/$orig $out \
   --progress \
   --prune-empty-dirs
-
-for network in data/raw/timeseries/XCP_FEB2020/sub-???/ses-t*/fcon/*
-do cp -r $network data/raw/timeseries
+	for atlas in data/raw/timeseries/$gsig/$orig/sub-???/ses-t*/fcon/*; do
+		cp -r $atlas $out
+	done
+	rm -r $out/$orig
 done
-
-rm -r data/raw/timeseries/XCP_FEB2020
