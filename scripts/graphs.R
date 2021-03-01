9d5ce0e6ad9f5d0dcdd9ff94345b2bbc270e628d
soffiafdz <so1.618e@gmail.com>
2021-02-28 21:08:49 -0500

    PRE-RUN
    Finish implementing new version of brainGraphList.
    Output should be a listception. Will send to ADA and wait for bugs.

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -2,140 +2,63 @@
 ## Set-up Parallelization
 doMC::registerDoMC(13)
 
-# New lines
-## Packages
-library("foreach")
-library("doParallel")
-library("plyr")
-library("readr")
+#library("foreach")
+#library("doParallel")
+#library("plyr")
 library("purrr")
 library("brainGraph")
 library("here")
 
-## Load rds objects
-#power264 <- read_rds(here("data/processed/rds/power264.rds"))
-covars      <- read_rds(here("data/processed/rds/covars.rds"))
-inds        <- read_rds(here("data/processed/rds/inds.rds"))
-thresholds  <- read_rds(here("data/processed/rds/thresholds.rds"))
-mats        <- read_rds(here("data/processed/rds/mats.rds"))
+## Load rds objects and schaefer parcellations
+covars      <- readr::read_rds(here("data/processed/rds/covars.rds"))
+#inds        <- readr::read_rds(here("data/processed/rds/inds.rds"))
+thresholds  <- readr::read_rds(here("data/processed/rds/thresholds.rds"))
+mats        <- readr::read_rds(here("data/processed/rds/mats.rds"))
 
-groups      <- c("Sham", "Real")
-sessions    <- c("T0", "T1", "T2", "T3")
+#grps        <- c("Sham", "Real")
+#sessions    <- c("T0", "T1", "T2", "T3")
 
-g_wrapper   <- function(input) {
+source(here("scripts/schaefer_atlas.R"))
+
+## Functions for purrr iteration
+g_sub   <- function(input, thresholds) {
   A_sub  <- input$A.norm.sub
+  atlas  <- names(input)
+
+  g <- vector("list", length(thresholds))
+  for (i in seq_along(thresholds)) {
+    g[[i]]  <- make_brainGraphList(A_sub[[i]], atlas, modality = "fmri",
+                                  threshold = thresholds[[i]],
+                                  weighted = TRUE,
+                                  gnames = covars$Study.ID,
+                                  groups = covars$group)
+  }
+  return(g)
+}
+
+g_group   <- function(input, thresholds) {
   A_mean <- input$A.norm.mean
   atlas  <- names(input)
 
+  gg <- vector("list", length(thresholds))
+  for (i in seq_along(thresholds)) {
+    gg[[i]] <- make_brainGraphList(A_mean[[i]], atlas, modality = "fmri",
+                                   threshold = thresholds[[i]],
+                                   weighted = TRUE,
+                                   gnames = paste(covars[, session],
+                                                  covars[, group],
+                                                  sep = "_"))
+  }
+  return(gg)
 }
 
+## Set names to matrices for output
+names(mats) <- c("raw", "dens")
 
 ## Main
-for (i in seq_along(mats)) {
-  for (j in seq_along(mats[[i]])) {
-    for (k in seq_along(mats[[j]])) {
-      A_sub  <- mats[[i]][[j]][[k]]$A.norm.sub
-      A_mean <- mats[[i]][[j]][[k]]$A.norm.mean
-      atlas  <- names(mats[[i]][[j]][k])
+g  <- map2(mats, thresholds, ~ map_depth(.x, 2, g_sub, thresholds = .y))
+gg <- map2(mats, thresholds, ~ map_depth(.x, 2, g_group, thresholds = .y))
 
-
-
-    }
-  }
-}
-
-
-A_sub <- mats$A.norm.sub
-A_mean <- mats$A.norm.mean
-atlas <- "power264"
-
-# Small odd/even list for hack regarding the group/session issue in
-# the create_mats function.
-group_session <- list(seq(1,10,2), seq(2,10,2))
-
-# Loop
-gph_group <- gph <- fnames <- vector("list", length = length(sessions))
-for (s in seq_along(sessions)) {
-  gph_group[[s]] <- gph[[s]] <- fnames[[s]] <-
-    vector("list", length = length(groups))
-  for (g in seq_along(groups)) {
-    # Continue if T14-Real
-    if (s == 3 && g == 2) next
-    for (t in seq_along(thresholds)) {
-      # Progress message
-      message(sprintf(
-        "Threshold: %i/%i; Group: %i; Session: %i; %s",
-        t, length(thresholds), g, s, format(Sys.time(), "%H:%M:%S")
-      ))
-
-      # Parallelization
-      foreach (i = seq_along(inds[[s]][[g]])) %dopar% {
-        g_tmp <- graph_from_adjacency_matrix(A_sub[[t]][,, inds[[s]][[g]][i]],
-          mode = "undirected", diag = FALSE, weighted = TRUE
-        )
-        g_tmp <- set_brainGraph_attr(g_tmp, atlas, modality = "fmri",
-          threshold = thresholds[t],
-          subject = covars[
-            Group == groups[g] & Session == sessions[s],
-            Study.ID
-          ][i],
-          group = groups[g],
-          A = A_sub[[t]][,, inds[[s]][[g]][i]],
-          use.parallel = FALSE
-        )
-        write_rds(g_tmp,
-          here("data", "processed", "graphs",
-            sprintf("s%i_g%i_thr%02i_sub%02i%s", s, g, t, i, ".rds")
-          )
-        )
-      }
-    }
-
-    # Group mean weighted graphs
-    message(sprintf("Group %i; Session %i; %s", g, s, format(Sys.time(), "%H:%M:%S")))
-    gph_group[[s]][[g]] <- lapply(seq_along(thresholds), function(x)
-      graph_from_adjacency_matrix(A_mean[[x]][[group_session[[g]][s]]],
-        mode = "undirected", diag = FALSE, weighted = TRUE
-      )
-    )
-
-    gph_group[[s]][[g]] <- llply(seq_along(thresholds), function(x)
-      set_brainGraph_attr(gph_group[[s]][[g]][[x]], atlas, modality = "fmri",
-        threshold = thresholds[x], group = groups[g],
-        A = A_mean[[x]][[group_session[[g]][s]]], use.parallel = FALSE
-      ),
-      .parallel = TRUE
-    )
-  }
-}
-
-## Validity check and saving of graph object
-for (s in seq_along(sessions)) {
-  for (g in seq_along(groups)) {
-    gph[[s]][[g]] <- fnames[[s]][[g]] <-
-      vector("list", length = length(thresholds))
-    for (t in seq_along(thresholds)) {
-      fnames[[s]][[g]][[t]] <- list.files(
-        here("data", "processed", "graphs"),
-        sprintf("s%i_g%i_thr%02i.*", s, g, t),
-        full.names = TRUE
-      )
-      gph[[s]][[g]][[t]] <- map(fnames[[s]][[g]][[t]], read_rds)
-    }
-
-    is_equal <- all.equal(
-      map_chr(gph[[s]][[g]][[1]], graph_attr, "name"),
-      covars[Group == groups[g] & Session == sessions[s], Study.ID]
-    )
-    if (isTRUE(is_equal)) map(fnames[[s]][[g]], file.remove)
-  }
-}
-
-## Name graph object
-gph <- set_names(gph, sessions)
-gph <- map(gph, set_names, groups)
-gph <- map_depth(gph, 2, set_names, thresholds)
-
-## Save rds objects
-write_rds(gph, here("data/processed/rds/gph.rds"))
-write_rds(gph_group, here("data/processed/rds/gph_group.rds"))
+## Save rds objects (compressed)
+readr::write_rds(g, here("data/processed/rds/g.rds"), "gz", compression = 9L)
+readr::write_rds(gg, here("data/processed/rds/gg.rds"), "gz", compression = 9L)
