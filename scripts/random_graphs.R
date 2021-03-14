64a7e6c9dbc3937db8e32bfdb10f4d9f97f40dc2
Soffiafdz@LAVIS <so1.618e@gmail.com>
2021-03-14 17:45:20 -0600

    Removed future; All atlas; Testing

--- scripts/random_graphs.R
+++ scripts/random_graphs.R
@@ -1,37 +1,64 @@
 #!/usr/bin/env Rscript
 
 ## Packages
-library("readr")
-library("purrr")
-library("furrr")
-library("brainGraph")
-library("data.table")
+#library("readr")
+#library("purrr")
+suppressMessages(library("data.table"))
+suppressMessages(library("brainGraph"))
+#suppressMessages(library("furrr"))
 library("here")
 
+## BrainGraph options
+doMC::registerDoMC(20)
+#future::plan(multicore, workers = 20)
+options(bg.subject_id = "participant_id",
+        #bg.group = "group",
+        #bg.session = "session",
+        bg.progress = FALSE,
+        bg.ncpus = 20)
+
+rds_dir <- here("data/processed/rds")
+g_dir   <- here(rds_dir, "graphs")
+covars  <- readr::read_rds(here('data/processed/rds/covars.rds'))
+t_met   <- c("raw", "dens")
+gs      <- c("gs", "ngs")
+parcel  <- c("power264", "gordon333", paste0("schaefer", c(1,2,4), "00x7"))
+n_rand <- 250L
+
 ## Load rds graphs
-sessions <- read_rds(here("./data/processed/rds/sessions.rds"))
-gph <- read_rds(here("./data/processed/graphs/gph.rds"))
+for (i in t_met) {
+  for (j in gs) {
+    for (k in parcel) {
+      rname <- sprintf("%s_%s_%s_rand.rds", i, j, k)
+      rfile <- here(g_dir, rname)
+      if (file.exists(rfile)) {
+        message(sprintf("%s - %s already created",
+                         Sys.time(), rname))
+      } else {
+        gname <- sprintf("%s_%s_%s.rds", i, j, k)
+        message(sprintf("%s - Reading %s rds", Sys.time(), gname))
+        g <- readr::read_rds(here(g_dir, gname))
 
-## No of random networks
-no_rand <- 3e2
+        message(sprintf("%s - Changing group attribute", Sys.time()))
+        for (x in seq_along(g)) {
+          for (y in seq_along(g[[x]][[10]])) {
+            g[[x]][[10]][[y]]$"Group" <-
+              covars[, paste(session, group, sep="_")][y]
+          }
+        }
 
-## Set up multiprocessing
-future::plan(multicore, workers = 15)
+        message(sprintf("%s - Creating random graphs", Sys.time()))
+        outdir <- here(rds_dir, sprintf("rand/%s_%s_%s", i, j, k))
+        rand.g <- analysis_random_graphs(g, N = n_rand, savedir = outdir,
+                                         clustering = FALSE)
 
-## Remove linging group (T14-Real)
-gph$T14$Real <- NULL
+        readr::write_rds(rand.g, rfile)
+        rm(g, rand.g)
+      }
+    }
+  }
+}
 
-## Main
-rnets <- future_map2(gph, sessions,
-  ~ analysis_random_graphs(
-    .x,
-    N = no_rand,
-    # N = 2,
-    savedir = here("data", "processed", "graphs", "rand", .y),
-    clustering = FALSE
-  ),
-  .progress = TRUE
-)
 
 ## Start from extracting ALL/Read existing graphs instead of creating them...
 # rgraphs_all <- function (g.list, N = 100, savedir = ".", ...) {
@@ -129,6 +156,3 @@ rnets <- future_map2(gph, sessions,
 #     clustering = FALSE
 #   ), .progress = TRUE
 # )
-
-## Save random_nets object
-write_rds(rnets, here("./data/processed/graphs/rnets.rds"))
